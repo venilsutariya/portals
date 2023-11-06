57257c09e34dbc4f57a353cabc85fc3cc24e75ab
VenilSutariya_ <venilsutariya7@gmail.com>
2023-11-06 21:07:39 +0530

    add code

--- components/customUi/HeaderImage.tsx
+++ components/customUi/HeaderImage.tsx
@@ -5,21 +5,21 @@ const HeaderImage = () => {
   return (
     <div className=" container my-5 pt-5">
       <div className=" row gap-3 px-3">
-        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
-          <img src="/images/img1.png" alt="" className="img-fluid h-100 w-100"/>
+        <div className=" col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+          <img src="/images/img1.png" alt="" className=" img-fluid h-100 w-100"/>
           <div className=" h-100 w-100 transition group-hover:bg-black/20 rounded-2xl absolute"></div>
         </div>
-        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+        <div className=" col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
           <img src="/images/img2.png" alt="" className=" img-fluid h-100 w-100" />
           <div className=" h-100 w-100 transition group-hover:bg-black/20  rounded-2xl absolute"></div>
         </div>
-        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+        <div className=" col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
           <img src="/images/img3.png" alt="" className=" img-fluid h-100 w-100" />
           <div className=" h-100 w-100 transition group-hover:bg-black/20  rounded-2xl absolute"></div>
         </div>
       </div>
       <div className="row my-4">
-        <div className="col-12 col-xl-8 transition group relative p-0">
+        <div className=" col-12 col-xl-8 transition group relative p-0">
           <img src="/images/img4.png" alt="" className=" img-fluid h-100 w-100" />
           <div className=" h-100 w-100 transition group-hover:bg-black/20 rounded-2xl absolute top-0"></div>
         </div>
@@ -30,7 +30,7 @@ const HeaderImage = () => {
             <div className=" mb-4">of work</div>
           </div>
           <div className=" my-5 transition group relative">
-            <img src="/images/img5.png" alt="" className="h-100 w-100"/>
+            <img src="/images/img5.png" alt="" className=" h-100 w-100"/>
             <div className=" h-100 w-100 transition group-hover:bg-black/20  rounded-2xl absolute top-0"></div>
           </div>
           <div className=" flex justify-between items-center mt-5">
