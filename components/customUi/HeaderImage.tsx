6ba77d6f4940d7827e775096c49e1478cfb93ea6
venilsutariya <venilsutariya7@gmail.com>
2023-11-09 12:36:44 +0530

    add code

--- components/customUi/HeaderImage.tsx
+++ components/customUi/HeaderImage.tsx
@@ -1,69 +1,65 @@
-"use client";
-
 import Link from "next/link";
-import { BsArrowRight } from 'react-icons/bs';
-import { useScroll, motion, useTransform } from "framer-motion";
-import { useRef } from 'react'; 
+import { BsArrowRight } from "react-icons/bs";
 
 const HeaderImage = () => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["0 1", "0.8 1"],
-  });
-
-  const scaleProgress = useTransform(scrollYProgress, [0,1], [0.9, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0,1], [0, 1]);
-
   return (
     <div className=" container my-5 pt-5">
-      <div className=" row gap-3 px-3">
-        <div className=" animatediv col-12 mt-3  group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
-          <img src="/images/img1.png" alt="" className=" img-fluid h-100 w-100" />
-          <div style={{ transition: 'all 0.3s' }} className=" h-100 w-100 transition group-hover:bg-black/20 rounded-2xl absolute"></div>
+      <div className=" row gap-3 p-3">
+        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+          <img
+            src="/images/img1.png"
+            alt=""
+            className="img-fluid h-100 w-100"
+          />
+          <div style={{transition: 'all 0.5s ease-out'}} className="hidden xl:block h-100 w-100 group-hover:bg-black/20  rounded-2xl absolute"></div>
         </div>
-        <div className=" animatediv col-12 mt-3 group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
-          <img src="/images/img2.png" alt="" className=" img-fluid h-100 w-100" />
-          <div style={{ transition: 'all 0.3s' }} className=" h-100 w-100 group-hover:bg-black/20  rounded-2xl absolute"></div>
+        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+          <img
+            src="/images/img2.png"
+            alt=""
+            className=" img-fluid h-100 w-100"
+          />
+          <div style={{transition: 'all 0.5s ease-out'}} className="hidden xl:block h-100 w-100 group-hover:bg-black/20  rounded-2xl absolute"></div>
         </div>
-        <div className=" animatediv col-12 mt-3 group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
-          <img src="/images/img3.png" alt="" className=" img-fluid h-100 w-100" />
-          <div style={{ transition: 'all 0.3s' }} className=" h-100 w-100 group-hover:bg-black/20  rounded-2xl absolute"></div>
+        <div className="col-12 mt-3 transition group mt-lg-0 col-lg flex relative h-100 w-100 p-0 justify-center">
+          <img
+            src="/images/img3.png"
+            alt=""
+            className=" img-fluid h-100 w-100"
+          />
+          <div style={{transition: 'all 0.5s ease-out'}} className="hidden xl:block h-100 w-100 group-hover:bg-black/20  rounded-2xl absolute"></div>
         </div>
       </div>
-      <motion.div className="row my-4" ref={ref} style={{scale: scaleProgress}}>
-        <div className=" col-12 col-xl-8 group relative p-0 animatediv">
-          <img src="/images/img4.png" alt="" className=" img-fluid h-100 w-100" />
-          <div style={{ transition: 'all 0.3s' }} className=" h-100 w-100 group-hover:bg-black/20 rounded-2xl absolute top-0"></div>
+      <div className="row p-1 my-4 gap-3">
+        <div className="col-12 col-xl-8 transition group relative p-0">
+          <img
+            src="/images/newimg.png"
+            alt=""
+            className=" img-fluid h-100 w-100"
+          />
+          <div style={{transition: 'all 0.5s ease-out', borderRadius: '33px'}} className="hidden xl:block h-100 w-100 group-hover:bg-black/20 absolute top-0 mediaimage"></div>
         </div>
-        <div className="col-12 col-xl-4">
-          <div className=" uppercase text-4xl lg:text-6xl mt-3 mt-xl-0">
-            <div className=" mb-3">our</div>
-            <div className=" mb-2">gallary</div>
-            <div className=" mb-4">of work</div>
-          </div>
-          <div className="w-100 my-5 group relative p-0">
-            <img src="/images/img5.png" alt="" className="img-fluid" />
-          </div>
-          <div className=" flex lg:justify-between items-center mt-5">
-            <Link
-              href={"/contact"}
-              className="pointer font-extralight text-sm lg:text-lg mr-3 lg:mr-3 uppercase flex justify-center items-center hover:bg-opacity-90 no-underline text-white rounded-full px-4 py-3 border-0 transition-colors"
-              style={{ border: "1px solid blue", backgroundColor: '#002DF0'}}
-            >
-              <img src="/images/CONTACT -_.png" alt="" />
-            </Link>
-            <Link
-              href={"/contact"}
-              className="pointer font-extralight text-sm lg:text-lg uppercase flex justify-center items-center no-underline text-black rounded-full px-4 py-3 hover:bg-blue-700/10 transition"
-              style={{ border: "1px solid blue" }}
-            >
-              <img src="/images/View more -_.png" alt="" />
-            </Link>
+        <div className="col">
+          <div className="row gap-3 p-4 p-lg-0">
+            <div className=" col-12 mb-3 relative group p-0">
+              <img
+                src="/images/newimg2.png"
+                alt=""
+                className=" img-fluid h-100 w-100"
+              />
+              <div style={{transition: 'all 0.5s ease-out'}} className=" hidden xl:block h-100 w-100 rounded-2xl group-hover:bg-black/20 absolute top-0"></div>
+            </div>
+            <div className=" col-12 relative group p-0">
+              <img
+                src="/images/blockchain.png"
+                alt=""
+                className=" img-fluid h-100 w-100"
+              />
+              <div style={{transition: 'all 0.5s ease-out'}} className=" hidden xl:block h-100 w-100 rounded-2xl group-hover:bg-black/20 absolute top-0"></div>
+            </div>
           </div>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
