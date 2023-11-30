2caf420e0c86b9d94d1fa658355b7eddd5c9e921
VenilSutariya_ <venilsutariya7@gmail.com>
2023-11-30 16:06:45 +0530

    add code

--- components/customUi/NavbarComp.tsx
+++ components/customUi/NavbarComp.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { GoDotFill } from "react-icons/go";
 
 const NavbarComp = () => {
@@ -15,10 +16,10 @@ const NavbarComp = () => {
               </div>
               <span className=" bg-gradient-to-b from-transparent via-violet-400 to-transparent w-[1px] mx-4"></span>
               <div className=" flex justify-center items-center">
-                <div className=" text-violet-300 me-3 text-lg">How  it Works</div>
-                <div className=" text-violet-300 me-3 text-lg">Work</div>
-                <div className=" text-violet-300 me-3 text-lg">Plans</div>
-                <div className=" text-violet-300 me-3 text-lg">FAQ</div>
+                <Link href={'/'} className=" text-violet-300 me-3 text-lg no-underline">How  it Works</Link>
+                <Link href={'/'} className=" text-violet-300 me-3 text-lg no-underline">Work</Link>
+                <Link href={'/'} className=" text-violet-300 me-3 text-lg no-underline">Plans</Link>
+                <Link href={'/'} className=" text-violet-300 me-3 text-lg no-underline">FAQ</Link>
               </div>
               <span className=" bg-gradient-to-b from-transparent via-violet-400 to-transparent w-[1px] mx-4"></span>
               <button className=" text-violet-300 text-lg border-1 border-violet-600/30 rounded-full px-3 py-1">Book a call</button>
@@ -29,9 +30,9 @@ const NavbarComp = () => {
               <span>The Only <span className="" style={{ color: '#C77DFF' }}>Design & Development </span></span> <span>service you will ever need</span>
             </div>
             <p className=" text-white mt-3 font-bold text-lg">Design & development plans for creators who want a no-BS edge.</p>
-            <div className="mt-3">
-              <button className=" text-white font-semibold text-2xl border-1 border-violet-600/30 rounded-full px-4 py-2 bg-violet-600/10">View plans</button>
-            </div>
+            <button className="viewplan">
+              <span className="text">View plans</span>
+            </button>
           </div>
         </div>
       </div>
