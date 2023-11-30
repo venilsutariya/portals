b6569c645e0cedc614f88503eba2b6fdde94a7ec
VenilSutariya_ <venilsutariya7@gmail.com>
2023-11-30 16:14:16 +0530

    add code

--- components/customUi/NavbarComp.tsx
+++ components/customUi/NavbarComp.tsx
@@ -30,9 +30,9 @@ const NavbarComp = () => {
               <span>The Only <span className="" style={{ color: '#C77DFF' }}>Design & Development </span></span> <span>service you will ever need</span>
             </div>
             <p className=" text-white mt-3 font-bold text-lg">Design & development plans for creators who want a no-BS edge.</p>
-            <button className="viewplan">
-              <span className="text">View plans</span>
-            </button>
+            <div className="mt-3">
+              <button className=" text-white font-semibold text-2xl border-1 border-violet-600/30 rounded-full px-4 py-2 bg-violet-600/10">View plans</button>
+            </div>
           </div>
         </div>
       </div>
