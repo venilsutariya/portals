78e94d806a422f1e6462ff62f1109af57b0d7548
VenilSutariya_ <venilsutariya7@gmail.com>
2023-12-04 15:06:33 +0530

    add code

--- components/customUi/HeadMain.tsx
+++ components/customUi/HeadMain.tsx
@@ -11,10 +11,10 @@ const HeadMain = () => {
           <div className="h-full flex flex-col justify-center text-center">
             <div className=" absolute top-64 flex justify-center w-full flex-col">
               <div className=" xl:text-6xl text-4xl md:text-5xl font-bold text-white flex flex-col gap-y-4">
-                <span>The Only <span className="" style={{ color: '#C77DFF' }}>Design & Development </span></span> <span className="lg:text-5xl text-2xl">service you will ever need</span>
+                <span>The Only <span className="" style={{ color: '#C77DFF' }}>Design & Development </span></span> <span className="lg:text-5xl text-3xl">service you will ever need</span>
               </div>
-              <p className=" text-white mt-3 font-bold text-lg px-4 md:p-0">Design & development plans for creators who want a no-BS edge.</p>
-              <div className="mt-5">
+              <p className=" text-white mt-3 font-bold text-lg">Design & development plans for creators who want a no-BS edge.</p>
+              <div className="mt-3">
                 <button className=" text-white font-semibold text-2xl border-1 border-violet-600/30 rounded-full px-4 py-2 bg-violet-600/10">View plans</button>
               </div>
             </div>
--- components/customUi/Navbar.tsx
+++ components/customUi/Navbar.tsx
@@ -13,17 +13,17 @@ const NavbarComp = () => {
                     <div className="fixed w-full top-11 flex justify-center items-center">
                         <div className=" flex py-2 rounded-full text-2xl">
                             <div className=" text-white font-semibold flex">
-                                <div className="flex justify-center items-center"><span className=" text-xl">PORTALS</span><span style={{ color: 'blue' }}>.</span></div>
+                                <div className="flex justify-center items-center"><span className="">PORTALS</span><span style={{ color: 'blue' }}>.</span></div>
                             </div>
                             <span className=" bg-gradient-to-b from-transparent via-violet-400 to-transparent w-[1px] mx-3"></span>
                             <div className=" flex justify-center items-center">
-                                <Link href={'/'} className=" text-purple-300 me-3 text-base no-underline">How  it Works</Link>
-                                <Link href={'/'} className=" text-purple-300 me-3 text-base no-underline">Work</Link>
-                                <Link href={'/'} className=" text-purple-300 me-3 text-base no-underline">Plans</Link>
-                                <Link href={'/'} className=" text-purple-300 me-3 text-base no-underline">FAQ</Link>
+                                <Link href={'/'} className=" text-purple-300 me-3 text-lg no-underline">How  it Works</Link>
+                                <Link href={'/'} className=" text-purple-300 me-3 text-lg no-underline">Work</Link>
+                                <Link href={'/'} className=" text-purple-300 me-3 text-lg no-underline">Plans</Link>
+                                <Link href={'/'} className=" text-purple-300 me-3 text-lg no-underline">FAQ</Link>
                             </div>
                             <span className=" bg-gradient-to-b from-transparent via-violet-400 to-transparent w-[1px] mx-3"></span>
-                            <button className=" text-purple-300 border-1 border-violet-600/30 text-base rounded-full px-3 py-1">Book a call</button>
+                            <button className=" text-purple-300 text-lg border-1 border-violet-600/30 rounded-full px-3 py-1">Book a call</button>
                         </div>
                     </div>
                 </div>
