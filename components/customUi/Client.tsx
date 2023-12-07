4922df49c9922c02f52b54b30fe0f4aae8570fd6
VenilSutariya_ <venilsutariya7@gmail.com>
2023-12-07 13:01:47 +0530

    add code

--- components/customUi/Client.tsx
+++ components/customUi/Client.tsx
@@ -2,7 +2,7 @@ const Client = () => {
     return (
         <>
             <div className="text-center py-5 border-0">
-                <div className=" uppercase font-semibold text-2xl mb-3 text-black/80"><span className=" me-3">T R U S T E D</span><span>B Y</span></div>
+                <div className=" uppercase font-semibold text-2xl text-black/80 mb-5"><span className=" me-3">T R U S T E D</span><span>B Y</span></div>
                 <div className=" py-5 container">
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5 gap-y-5">
                         <div className="col flex justify-center mb-5 mb-lg-0">
