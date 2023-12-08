3f58ab49c1bc472d5c231386971291a351f7e53a
VenilSutariya_ <venilsutariya7@gmail.com>
2023-12-08 13:59:47 +0530

    add code

--- components/customUi/WhatYouGet.tsx
+++ components/customUi/WhatYouGet.tsx
@@ -2,13 +2,13 @@ const WhatYouGet = () => {
     return (
         <div className=" container mt-52 pb-44">
             <div className=" row row-cols-1 row-cols-lg-2 p-0 g-0">
-                <div className=" col w-full">
+                <div className=" col">
                     <div className="lg:text-4xl w-full text-3xl font-semibold flex justify-start">
                         What “exactly” <br />
                         you will get ?
                     </div>
                 </div>
-                <div className=" col flex justify-end w-full">
+                <div className=" col flex justify-end">
                     <div className=" text-2xl text-black lg:flex lg:flex-col">
                         <div className="">We will provide many things, but here</div> 
                         <div className="">are some things at we excel most.</div>
