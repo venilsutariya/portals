075803ab08e92ad7d1431bade422536276ff5164
VenilSutariya_ <venilsutariya7@gmail.com>
2023-12-08 13:50:27 +0530

    add code

--- components/customUi/WhatYouGet.tsx
+++ components/customUi/WhatYouGet.tsx
@@ -8,8 +8,8 @@ const WhatYouGet = () => {
                         you will get ?
                     </div>
                 </div>
-                <div className=" col flex lg:justify-end w-full">
-                    <div className=" text-2xl text-black lg:flex lg:flex-col mt-10 lg:mt-0">
+                <div className=" col flex lg:justify-end w-full mb-10 lg:mb-0">
+                    <div className=" text-2xl text-black lg:flex lg:flex-col">
                         <div className="">We will provide many things, but here</div> 
                         <div className="">are some things at we excel most.</div>
                     </div>
