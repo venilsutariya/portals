e12443bd9e88f259c8169886b57f052d9a031995
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-12-19 10:49:14 +0530

    add code

--- components/customUi/Project.tsx
+++ components/customUi/Project.tsx
@@ -1,28 +1,27 @@
 "use client";
 
-import VisibilitySensor from 'react-visibility-sensor';
+import VisibilitySensor from "react-visibility-sensor";
 import { useEffect, useState } from "react";
 
 const Project = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-    const [isVisible, setIsVisible] = useState<boolean>(false);
+  const onVisibilityChange = (visible: boolean) => {
+    if (visible) {
+      setIsVisible(true);
+    }
+  };
 
-    const onVisibilityChange = (visible: boolean) => {
-        if (visible) {
-            setIsVisible(true);
-        }
+  useEffect(() => {
+    // Reset the isVisible state when the component unmounts or when you leave the section
+    return () => {
+      setIsVisible(false);
     };
+  }, []);
 
-    useEffect(() => {
-        // Reset the isVisible state when the component unmounts or when you leave the section
-        return () => {
-            setIsVisible(false);
-        };
-    }, []);
-
-    return (
-        <div className="mx-auto" style={{ backgroundColor: '#000212' }}>
-            {/* <VisibilitySensor onChange={onVisibilityChange} partialVisibility>
+  return (
+    <div className="mx-auto" style={{ backgroundColor: "#000212" }}>
+      {/* <VisibilitySensor onChange={onVisibilityChange} partialVisibility>
                 <div className={`row mb-5 gap-y-5 p-0 g-0 ${isVisible ? 'animate__animated animate__slideInUp' : ''}`}>
                     <div className="col-12 col-lg px-2">
                         <img src="/images/project1.png" className="img-fluid" alt="" />
@@ -48,10 +47,20 @@ const Project = () => {
                     </div>
                 </div>
             </VisibilitySensor> */}
-            {/* @ts-ignore */}
-            <div style={{height: "600px"}}> <iframe src="https://www.playbook.com/e/jenilthumar/aus1HghNQ5dihmRUpppUgrBs?theme=carousel&rows=1&slidesPerRow=3" title="Portals Work - Playbook.com" sandbox="allow-same-origin allow-scripts" frameborder="0" width="100%" height="100%" ></iframe> </div>
-        </div>
-    );
-}
+      {/* @ts-ignore */}
+      <div style={{height: "600px"}}>
+        <iframe
+          src="https://www.playbook.com/e/jenilthumar/aus1HghNQ5dihmRUpppUgrBs?theme=gallery"
+          title="Portals Work - Playbook.com"
+          sandbox="allow-same-origin allow-scripts"
+          frameborder="0"
+          width="100%"
+          height="100%"
+          color="#000212"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
 
 export default Project;
