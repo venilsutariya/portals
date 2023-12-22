d8e9bdd6dca9307f28a828b73063e0681bf8ff6d
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-12-22 10:23:41 +0530

    add code

--- components/customUi/WhyChoose.tsx
+++ components/customUi/WhyChoose.tsx
@@ -28,49 +28,50 @@ const WhyChoose = () => {
                     <div className="lg:px-32">
                         <div className=" bg-gradient-to-r from-transparent via-blue-200/30 to-transparent lg:p-[0.1px] xl:p-[0.3px]  my-10"></div>
                     </div>
-                    <div className=" px-2 flex justify-between">
-                        <div></div>
-                        <div className=" row container flex row-cols-1 row-cols-lg-3 gap-y-10 pb-5 px-0 g-0">
-                            <div className=" col flex tracking-wider gap-x-4">
-                                <span><img src="/images/icon1.png" alt="" /></span>
-                                <div>
-                                    <span style={{ color: '#fff' }}>Fast.</span>
-                                    <span className=" text-white/50">We promise <br /> faster designs</span>
+                    <div className="">
+                        <div className='whychoosediv'>
+                            <div className=" row container  flex row-cols-1 row-cols-lg-3 gap-y-10 pb-5 px-0 g-0">
+                                <div className=" col flex tracking-wider gap-x-4">
+                                    <span><img src="/images/icon1.png" alt="" /></span>
+                                    <div>
+                                        <span style={{ color: '#fff' }}>Fast.</span>
+                                        <span className=" text-white/50">We promise <br /> faster designs</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=" col flex tracking-wider gap-x-4">
-                                <span><img src="/images/icon4.png" alt="" /></span>
-                                <div className=" flex flex-col">
-                                    <span style={{ color: '#fff' }}>Real development.</span>
-                                    <span className=" text-white/50">Not just no-code</span>
+                                <div className=" col flex tracking-wider gap-x-4">
+                                    <span><img src="/images/icon4.png" alt="" /></span>
+                                    <div className=" flex flex-col">
+                                        <span style={{ color: '#fff' }}>Real development.</span>
+                                        <span className=" text-white/50">Not just no-code</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=" col flex tracking-wider gap-x-4">
-                                <span><img src="/images/icon2.png" alt="" /></span>
-                                <div className=" flex flex-col">
-                                    <span style={{ color: '#fff' }}>Worry free pricing.</span>
-                                    <span className=" text-white/50">monthly flat fee.</span>
+                                <div className=" col flex tracking-wider gap-x-4">
+                                    <span><img src="/images/icon2.png" alt="" /></span>
+                                    <div className=" flex flex-col">
+                                        <span style={{ color: '#fff' }}>Worry free pricing.</span>
+                                        <span className=" text-white/50">monthly flat fee.</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=" col flex tracking-wider gap-x-4">
-                                <span><img src="/images/icon3.png" alt="" /></span>
-                                <div className=" flex flex-col">
-                                    <span style={{ color: '#fff' }}>Unlimited members.</span>
-                                    <span className=" text-white/50">On-board your team.</span>
+                                <div className=" col flex tracking-wider gap-x-4">
+                                    <span><img src="/images/icon3.png" alt="" /></span>
+                                    <div className=" flex flex-col">
+                                        <span style={{ color: '#fff' }}>Unlimited members.</span>
+                                        <span className=" text-white/50">On-board your team.</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=" col flex  tracking-wider gap-x-4">
-                                <span className=''><img src="/images/icon5.png" alt="" /></span>
-                                <div className=" flex flex-col">
-                                    <span style={{ color: '#fff' }}>Pause anytime.</span>
-                                    <span className=" text-white/50">Resume  whenever <br /> you want.</span>
+                                <div className=" col flex  tracking-wider gap-x-4">
+                                    <span className=''><img src="/images/icon5.png" alt="" /></span>
+                                    <div className=" flex flex-col">
+                                        <span style={{ color: '#fff' }}>Pause anytime.</span>
+                                        <span className=" text-white/50">Resume  whenever <br /> you want.</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=" col flex tracking-wider gap-x-4">
-                                <span><img src="/images/icon6.png" alt="" /></span>
-                                <div className=" flex flex-col">
-                                    <span style={{ color: '#fff' }}>Slack/Trello support.</span>
-                                    <span className=" text-white/50">Allocated workspace.</span>
+                                <div className=" col flex tracking-wider gap-x-4">
+                                    <span><img src="/images/icon6.png" alt="" /></span>
+                                    <div className=" flex flex-col">
+                                        <span style={{ color: '#fff' }}>Slack/Trello support.</span>
+                                        <span className=" text-white/50">Allocated workspace.</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
