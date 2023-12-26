d5864c49137b180017a06995bda399eaa15d4ba1
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-12-26 21:10:00 +0530

    add code

--- components/customUi/Project.tsx
+++ components/customUi/Project.tsx
@@ -39,120 +39,58 @@ const Project = () => {
     <>
     <div ref={innerDivRef} className="scrollbar-hide" style={{ backgroundColor: "#000212", overflowY: "hidden", overflowX: "auto" }}>
       <VisibilitySensor onChange={onVisibilityChange} partialVisibility>
-        <div className="md:block hidden">
+        <div className="">
           <div className="d-flex gap-x-5 p-3">
             <img
-              src="/images/image-1.png"
-              alt="image"
-              className=" img-fluid  transform rounded-3xl sm:ms-[-7rem] md:ms-[-10rem] lg:ms-[-17rem]"
-              style={{ transform: "rotate(-1deg) scale(0.6)" }}
-            />
-            <img
-              src="/images/image-2.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem]  transform rounded-3xl rotate-1"
-              style={{ transform: "rotate(1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-3.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem]  transform rounded-3xl"
-              style={{ transform: "rotate(-1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-4.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl rotate-1"
-              style={{ transform: "rotate(1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-5.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl"
-              style={{ transform: "rotate(-1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-6.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl rotate-1"
-              style={{ transform: "rotate(1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-7.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl"
-              style={{ transform: "rotate(-1deg) scale(0.6)" }}
-              />
-            <img
-              src="/images/image-8.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl rotate-1"
-              style={{ transform: "rotate(1deg) scale(0.6)" }}
-            />
-            <img
-              src="/images/image-9.png"
-              alt="image"
-              className=" object-cover sm:ms-[-15rem] md:ms-[-20rem] lg:ms-[-30rem] transform rounded-3xl rotate-1"
-              style={{ transform: "rotate(-1deg) scale(0.6)" }}
-            />
-          </div>
-        </div>
-      </VisibilitySensor>
-    </div>
-    <div ref={innerDivRef} className="scrollbar-hide" style={{ backgroundColor: "#000212", overflowY: "hidden", overflowX: "auto" }}>
-      <VisibilitySensor onChange={onVisibilityChange} partialVisibility>
-        <div className="md:hidden block">
-          <div className="d-flex gap-x-5 p-3">
-            <img
-              src="/images/image-1.png"
+              src="/images/image_1.png"
               alt="image"
               className=" img-fluid  transform rounded-3xl "
               style={{ transform: "rotate(-1deg)" }}
             />
             <img
-              src="/images/image-2.png"
+              src="/images/image_2.png"
               alt="image"
               className=" object-cover transform rounded-3xl rotate-1"
               style={{ transform: "rotate(1deg)" }}
               />
             <img
-              src="/images/image-3.png"
+              src="/images/image_3.png"
               alt="image"
               className=" object-cover transform rounded-3xl"
               style={{ transform: "rotate(-1deg)" }}
               />
             <img
-              src="/images/image-4.png"
+              src="/images/image_4.png"
               alt="image"
               className=" object-cover transform rounded-3xl rotate-1"
               style={{ transform: "rotate(1deg)" }}
               />
             <img
-              src="/images/image-5.png"
+              src="/images/image_5.png"
               alt="image"
               className=" object-cover transform rounded-3xl"
               style={{ transform: "rotate(-1deg)" }}
               />
             <img
-              src="/images/image-6.png"
+              src="/images/image_6.png"
               alt="image"
               className=" object-cover transform rounded-3xl rotate-1"
               style={{ transform: "rotate(1deg)" }}
               />
             <img
-              src="/images/image-7.png"
+              src="/images/image_7.png"
               alt="image"
               className=" object-cover transform rounded-3xl"
               style={{ transform: "rotate(-1deg)" }}
               />
             <img
-              src="/images/image-8.png"
+              src="/images/image_8.png"
               alt="image"
               className=" object-cover transform rounded-3xl rotate-1"
               style={{ transform: "rotate(1deg)" }}
             />
             <img
-              src="/images/image-9.png"
+              src="/images/imag_9.png"
               alt="image"
               className=" object-cover transform rounded-3xl rotate-1"
               style={{ transform: "rotate(-1deg)" }}
