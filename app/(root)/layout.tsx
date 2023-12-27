6c8c9134d2ebbc400a34e3d5c4e21627d02488a3
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-12-27 12:07:49 +0530

    add code

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,10 @@
-"use client";
-
 import NavbarComp from "@/components/customUi/Navbar";
-import { useEffect } from "react";
-import { initializeSmoothScroll } from "@/utils/smoothScroll";
 
 const RootLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
-
-    useEffect(() => {
-        initializeSmoothScroll();
-    }, []);
 
     return (
         <div className=" relative">
--- utils/smoothScroll.js
+++
@@ -1,9 +0,0 @@
-// utils/smoothScroll.js
-import SmoothScroll from "smooth-scroll";
-
-export function initializeSmoothScroll() {
-  new SmoothScroll('a[href*="#"]', {
-    speed: 800,
-    easing: "easeInOutCubic",
-  });
-}
