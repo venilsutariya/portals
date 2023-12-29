b3aca5835130978d036d124eab02c63a067cb54d
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-12-29 10:11:50 +0530

    add code

--- components/customUi/MouseShadow.tsx
+++ components/customUi/MouseShadow.tsx
@@ -1,48 +1,78 @@
 "use client";
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, MouseEventHandler } from 'react';
 
 interface CursorPosition {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 const CursorFollower: React.FC = () => {
-  const cursorRef = useRef<HTMLDivElement | null>(null);
+    const cursorRef = useRef<HTMLDivElement | null>(null);
+    const [isLeftClicked, setIsLeftClicked] = useState(false);
 
-  const updateCursorPosition = (e: MouseEvent) => {
-    if (cursorRef.current) {
-      cursorRef.current.style.left = e.clientX + 'px';
-      cursorRef.current.style.top = e.clientY + 'px';
-    }
-  };
-
-  const updateCursorPositionWithAnimationFrame = (e: MouseEvent) => {
-    requestAnimationFrame(() => updateCursorPosition(e));
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousemove', updateCursorPositionWithAnimationFrame);
-
-    return () => {
-      document.removeEventListener('mousemove', updateCursorPositionWithAnimationFrame);
+    const updateCursorPosition = (e: MouseEvent) => {
+        if (cursorRef.current) {
+            cursorRef.current.style.left = e.clientX + 'px';
+            cursorRef.current.style.top = e.clientY + 'px';
+        }
     };
-  }, []);
 
-  return (
-    <div
-      ref={cursorRef}
-      style={{
-        position: 'fixed',
-        width: '00px',
-        height: '00px',
-        boxShadow: '1px 1px 50px 10px #fff',
-        borderRadius: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: '9999999'
-      }}
-    ></div>
-  );
+    const handleClick: MouseEventHandler<HTMLDivElement> = () => {
+        setIsLeftClicked(true);
+        setTimeout(() => {
+            setIsLeftClicked(false);
+        }, 500); // Adjust the duration of the left-click animation
+    };
+
+    const updateCursorPositionWithAnimationFrame = (e: MouseEvent) => {
+        requestAnimationFrame(() => updateCursorPosition(e));
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousemove', updateCursorPositionWithAnimationFrame);
+
+        return () => {
+            document.removeEventListener('mousemove', updateCursorPositionWithAnimationFrame);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (isLeftClicked && cursorRef.current) {
+            cursorRef.current.classList.add('pop-animation');
+
+            const onAnimationEnd = () => {
+                if (cursorRef.current) {
+                    cursorRef.current.classList.remove('pop-animation');
+                }
+            };
+
+            cursorRef.current.addEventListener('animationend', onAnimationEnd);
+
+            return () => {
+                // @ts-ignore
+                cursorRef.current.removeEventListener('animationend', onAnimationEnd);
+            };
+        }
+    }, [isLeftClicked]);
+
+    return (
+        <div
+            className={`${isLeftClicked && 'mouseclick'}`}
+            ref={cursorRef}
+            onClick={handleClick}
+            // @ts-ignore
+            style={{
+                position: 'fixed',
+                width: '0px',
+                height: '0px',
+                boxShadow: '1px 1px 1000px 20px #fff',
+                borderRadius: '50%',
+                transform: 'translate(-50%, -50%)',
+                transition: 'all 0.1s',
+            }}
+        ></div>
+    );
 };
 
 export default CursorFollower;
